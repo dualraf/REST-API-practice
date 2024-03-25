6e9b85d43fd91aabe10935c7dfc71396f3d38b99
dualraf <zairhaziel@gmail.com>
2024-03-24 22:11:19 -0500

    routing from middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -17,6 +17,7 @@ export const isAuthenticated = async(req: express.Request, res: express.Response
 
         merge(req, {identity: existingUser});
 
+        return next();
     } catch(error){
         console.log(error);
         return res.sendStatus(400);
